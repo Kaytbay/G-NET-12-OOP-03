ab93e6fbe617c356c99263ce93b442242aecdc3e
Kaytbay <mohamedkaytbay@gmail.com>
2026-03-01 22:36:56 +0200

    Part1 Q1

--- oop 3/Program.cs
+++ oop 3/Program.cs
@@ -1,10 +1,52 @@
-﻿    namespace oop_3
+﻿namespace oop_3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region part1
+
+            #region Q01:
+            /*
+            a) A University has Departments. If the university is closed, the departments no longer exist.  
+            b) A Driver uses a Car. The driver does not own the car.
+            c) A Dog is an Animal. 
+            d) A Team has Players. If the team is deleted, the players still exist. 
+            e) A method receives a Logger as a parameter and calls it inside the method only.
+
+             */
+
+
+
+
+
+            // a) Composition (has a  but strong )
+
+
+            // b) Accociation  (use a )
+
+
+            // c) Inheritance  (is a )
+
+
+            // d) Aggregation (has a but weak )
+
+
+            //e) Dependency  ( method depend on the logger)
+
+            #endregion
+
+
+
+
+
+
+
+            #endregion
+
+
+
         }
     }
 }
